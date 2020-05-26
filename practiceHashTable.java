d6274eea6e1dce501cb4161cca259d60c601287c
Ken Wagner <ken_xwagner@hotmail.com>
2020-05-25 20:15:19 -0700

    Create practiceHashTable.java

---
+++ practiceHashTable.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class hashTableExample {
+  public static void main(String[] arg)
+  {
+    Hashtable<Integer, String> imaHash = new Hashtable<Integer, String>();
+    
+    
+    }
+    }
